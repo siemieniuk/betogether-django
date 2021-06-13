c6ab0e35aa12d811695989aec90299aff3b11b14
Szymon Siemieniuk <szymonsiemieniuk01@gmail.com>
2021-06-13 18:47:12 +0200

    Configured mysql

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,7 @@ FROM python:3
 ENV PYTHONUNBUFFERED=1
 WORKDIR /code
 COPY requirements.txt /code/
-RUN pip install -r requirements.txt
+RUN pip install --upgrade pip && \
+    pip install -r requirements.txt && \
+    pip install mysqlclient
 COPY . /code/
--- betogether/myproject/settings.py
+++ betogether/myproject/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-7$9o!dh^ur^xyf&2wo)i=2gj=#jowx!um2re_=!^$aqcote8fe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', ]
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -75,8 +74,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my-app-db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': 3306,
     }
 }
 
