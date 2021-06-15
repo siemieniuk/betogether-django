7a34bb5da16479dbbc2ae2ac377dfe4001aa87bd
Szymon Siemieniuk <szymonsiemieniuk01@gmail.com>
2021-06-15 20:29:30 +0200

    Added three new models: Comment, Post, Profile

--- betogether/myapp/models.py
+++ betogether/myapp/models.py
@@ -3,4 +3,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    time_created = models.DateTimeField(auto_now=True)
+
+class Post(models.Model):
+    author = models.ForeignKey('Profile', on_delete=models.SET_NULL, null=True)
+    time_created = models.DateTimeField(auto_now=True)
+    content = models.CharField(max_length=1000)
+
+class Comment(models.Model):
+    author = models.ForeignKey('Profile', on_delete=models.SET_NULL, null=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    time_created = models.DateTimeField(auto_now=True)
+    content = models.CharField(max_length=500)
