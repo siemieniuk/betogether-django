1bac0f0ca6ab674bc108eea6c6e44f189fcae5a2
Szymon Siemieniuk <szymonsiemieniuk01@gmail.com>
2021-06-27 12:28:55 +0200

    Renamed views

--- betogether/myapp/urls.py
+++ betogether/myapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.homepage, name='homepage'),
     path('terms_and_conditions', views.terms, name='terms_and_conditions'),
     path('login', views.LoginView.as_view(), name='login'),
-    path('logout', views.logout_view),
-    path('register', views.register, name='register')
+    path('logout', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register', views.register, name='register'),
 ]
